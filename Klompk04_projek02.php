abf8d1ccb8b88d76073e5fe51bf719599921219e
kelompok444 <74642419+kelompok444@users.noreply.github.com>
2020-11-18 19:45:59 +0700

    Create Klompk04_projek02.php

---
+++ Klompk04_projek02.php
@@ -0,0 +1,56 @@
+<?php
+   //cara penulisan class dan property OOP PHP -@pulung_coding
+  class Kelinci{   //class Kelinci
+    
+    var $warna;  //property warna
+    var $jenis kelamin;  //property jenis kelamin
+    var $usia; //property usia
+  }
+
+
+ ?>
+
+
+
+   
+
+    
+   
+   
+    
+ 
+
+
+ 
+  
+
+    
+
+     
+
+     
+
+     
+
+     
+
+     
+
+     
+
+     
+
+  
+
+     
+
+      
+
+
+
+   
+
+   
+
+   
+
