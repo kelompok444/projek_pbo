13188d72dd0b4dc21088dba4ce973275aa5e8959
kelompok444 <74642419+kelompok444@users.noreply.github.com>
2020-11-18 19:54:06 +0700

    Create Klompk04_projek03.php

---
+++ Klompk04_projek03.php
@@ -0,0 +1,19 @@
+<?php
+   // Cara penulisan class dan property OOP PHP - @pulung_coding
+   class sepeda{
+    // property OOP
+     var $warna;
+     var $merek;
+     var $ukuran;
+     
+     // method OOP
+     function maju(){
+     //isi method
+  }
+     function berhenti(){
+      //isi method
+    }
+   
+   }
+   
+ ?>
